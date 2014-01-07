8ffe5d40852274204d255cf09bc83ea4b831ef35
Mateusz <mateusz@MacBook-Pro-Mateusz.local>
2014-01-07 18:23:28 +0100

    moduł odpowiedzialny za wielowątkowość (v1)

---
+++ Projekt/src/lifeconc.erl
@@ -0,0 +1,70 @@
+%% @author Mateusz Jabłoński
+%% @doc Moduł odpowiedzialny za rozproszenie obliczeń
+
+-module(lifeconc).
+-compile(export_all).
+
+prepareNodes() ->
+	Nodes = net_adm:world(),
+	io:format("Have all nodes~n"),
+	
+	loadModule(lifeconc, Nodes),
+	io:format("Module lifeconc loaded to all nodes~n"),
+	
+	loadModule(lifelogic, Nodes),
+	io:format("Module lifelogic loaded to all nodes~n"),
+	
+	M = lifeconc,
+	F = trolo,
+	A = [okeeeej],
+	
+	NodeKeyList = operateNodes(Nodes, M,F,A),
+	io:format("All nodes operated~n"),
+	
+	listen(NodeKeyList),
+	{ok, done}.
+
+% ładuje skompilowany kod modułu do podanych węzłów
+% maszyny remote nie mają dostępu do binarek
+
+loadModule(Module, Nodes) ->
+	{_Mod, Bin, Filename} = code:get_object_code(Module),
+	lists:map(fun(Node) -> rpc:call(Node, code, load_binary, [Module, Filename, Bin]) end, Nodes).
+
+% utworzenie na każdym węźle asynchronicznego procesu
+% zwraca słownik identyfikator_węzła:klucz_procesu
+
+operateNodes(Nodes, M,F,A) ->
+	Result = lists:map(fun(Node) -> {Node, rpc:async_call(Node, lifeconc, operateNode, [Node, self(), {M,F,A}])} end, Nodes),
+	Result.
+
+% nasłuchuje odpowiedzi z rozproszonych procesów
+% gdy dostanie wiadomość, wyświetla zwróconą przez proces wartość
+
+listen(NodeKeyList) -> 
+	receive
+		{ok, Node} ->
+			{_, Key} = lists:keyfind(Node, 1, NodeKeyList),
+			RemoteResult = rpc:yield(Key),
+			io:format("- Dostalem od ~s klucz ~p; wartosc: ", [Node, Key]),
+			
+			io:write(RemoteResult),
+			io:format("~n"),
+			listen(NodeKeyList)
+	after 5000 ->
+			io:format("Timeout~n"),
+			timeout
+	end.
+
+% metoda uruchomiona na pojedynczym węźle
+% dzieli zadanie na lokalne procesy, następnie łączy i zwraca wynik
+% powiadamia parent node'a o zakończeniu obliczeń
+
+operateNode(Node, ParentPid, {M,F,A}) ->
+	Res = erlang:apply(M, F, [self()]),
+	ParentPid ! {ok, Node},
+	Res.
+
+trolo(Var) ->
+	timer:sleep(100),
+	{localPid, Var}.
