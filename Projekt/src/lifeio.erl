d4edb3730ea393bdf0b0f132696585410834c058
Mateusz <mateusz@MacBook-Pro-Mateusz.local>
2014-01-13 12:26:35 +0100

    ladowanie zawsze lokalnego pliku do kolumn

--- Projekt/src/lifeio.erl
+++ Projekt/src/lifeio.erl
@@ -55,7 +55,8 @@ readDataToBoard(FileName) ->
 %% Wczytuje tablicę z pliku
 %% Dzieli ja na zadana liczbe column. Zwraca rozmiar calej tablicy liste kolumn i rozmiar kolumny(bez dwoch dodatkowych bitow)
 readDataToColumns(FileName, ColumnsCount) ->
-	{FD,Pow} = lifeRead(FileName),
+  {ok,Dir} = file:get_cwd(),
+	{FD,Pow} = lifeRead(Dir ++ '/' ++ FileName),
 	BoardSize = trunc(math:pow(2, Pow)),
 	Boards = readParts(FD, BoardSize*BoardSize, BoardSize,ColumnsCount),
 	{BoardSize, Boards}.
